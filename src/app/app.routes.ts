3d97f3870dba198865cb3ba82a2141d8b1af896f
omri.asulin <omri.asulin@passportcard.co.il>
2024-10-27 10:14:48 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ 
+    path: '', 
+    loadComponent: () => import('../app/components/home/home.component').then(m => m.HomeComponent)
+  },
+  ];
+
+  bootstrapApplication(AppComponent, {
+    providers: [provideRouter(routes)],
+  }).catch(err => console.error(err));
