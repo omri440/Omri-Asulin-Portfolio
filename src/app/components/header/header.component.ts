083e69bb374a31767a1380da2b72ac193b589e3a
omri.asulin <omri.asulin@passportcard.co.il>
2024-10-27 14:43:44 +0200

    finish header and add component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [NavbarComponent, MatIconModule, MatButtonModule,CommonModule],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  isMenuVisible: boolean = false;
+
+  toggleMenu() {
+    this.isMenuVisible = !this.isMenuVisible;
+  }
+}
---
+++ src/app/components/protfolio/protfolio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-protfolio',
+  standalone: true,
+  imports: [],
+  templateUrl: './protfolio.component.html',
+  styleUrl: './protfolio.component.css'
+})
+export class ProtfolioComponent {
+
+}
