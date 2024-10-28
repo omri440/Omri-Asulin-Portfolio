336ef25731d60fcf00939d1c238554ab3b0c4488
omri.asulin <omri.asulin@passportcard.co.il>
2024-10-28 17:27:24 +0200

    progress with the protfolio section added filter

--- src/app/components/protfolio/protfolio.component.ts
+++ src/app/components/protfolio/protfolio.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {MatDividerModule} from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-protfolio',
   standalone: true,
-  imports: [],
+  imports: [MatDividerModule,MatIconModule,CommonModule],
   templateUrl: './protfolio.component.html',
   styleUrl: './protfolio.component.css'
 })
 export class ProtfolioComponent {
+  public keyWord = "All"
 
+
+  onClickFilter(keyWord:string){
+    this.keyWord = keyWord;
+  }
 }
